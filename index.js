43fc0ec245dd6ae5902eef480c5e73fccf67927e
Petrik Gabos <pg.petrgabriel@gmail.com>
2023-02-05 13:38:31 +0100

    movielist done on web

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 'use strict';
 
-/* tady bude tvůj kód */
-console.log('JavaScript ve stránce funguje');
+const movieList = document.querySelector('#movies')
+
+const showMovies = (data) => {
+
+   movieList.innerHTML +=  `
+   <div class="movie">
+   <img class="movie__img" src=${data.posterUrl} alt=${data.title}>
+   <h2 class="movie__title">${data.title}</h2>
+   <p class="movie__year">${data.year}</p>
+   <p class="movie__genre">${data.genres.join(", ")}</p>
+ </div>   
+
+ `
+}
+
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
+    .then((resp) => resp.json())
+    .then((data => data.forEach(showMovies)))
+
+
+
+
+
+
+
