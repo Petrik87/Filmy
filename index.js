3d824bc910b0047dded314f0399be5b2237e3804
Petrik Gabos <pg.petrgabriel@gmail.com>
2023-03-10 11:48:37 +0100

    repair rollback

--- index.js
+++ index.js
@@ -2,26 +2,19 @@
 
 const movieList = document.querySelector('#movies')
 
-const showMovies = (data) => {
-
-   movieList.innerHTML +=  `
-   <div class="movie">
-   <img class="movie__img" src=${data.posterUrl} alt=${data.title}>
-   <h2 class="movie__title">${data.title}</h2>
-   <p class="movie__year">${data.year}</p>
-   <p class="movie__genre">${data.genres.join(", ")}</p>
- </div>   
-
- `
-}
-
 fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
     .then((resp) => resp.json())
-    .then((data => data.forEach(showMovies)))
+    .then((data) => (showMovies(data)))
 
-
-
-
-
-
-
+    const showMovies = (movieData) => {
+      
+      movieList.innerHTML =  `
+       <div class="movie">
+       <img class="movie__img" src=${movieData[0].posterUrl} alt=${movieData[0].title}>
+       <h2 class="movie__title">${movieData[0].title}</h2>
+       <p class="movie__year">${movieData[0].year}</p>
+       <p class="movie__genre">${movieData[0].genres.join(", ")}</p>
+     </div>   
+    
+     `
+     }
